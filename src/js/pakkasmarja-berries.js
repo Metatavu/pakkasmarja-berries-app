6ce87a7ea2432ddde331d87cb3e1a6f0cd8bcabc
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-06-15 14:22:07 +0300

    Bootstrapped application

---
+++ src/js/pakkasmarja-berries.js
@@ -0,0 +1,39 @@
+(function(){
+  'use strict';
+  
+  $.widget("custom.pakkasmarjaBerries", { 
+    _create : function() {
+      this.horizontalSwiper = new Swiper('.swiper-horizontal', {});
+      this.verticalSwiper = new Swiper('.swiper-vertical', {
+        scrollbar: '.swiper-scrollbar',
+        direction: 'vertical',
+        slidesPerView: 'auto',
+        mousewheelControl: true,
+        freeMode: true
+      });
+      this.horizontalSwiper.on('onSlideChangeEnd', (swiper) => {this._onSlideChangeEnd(swiper); });
+      $(this.element).find('.secondary-menu .menu-item').on('click', (event) => { this._onMenuItemClick(event); });
+    },
+    
+    _onSlideChangeEnd: function(swiper) {    
+      $('.secondary-menu')
+        .find('.menu-item')
+        .removeClass('active');
+      
+      $('.secondary-menu')
+        .find('.menu-item[data-slide-index="'+ swiper.activeIndex +'"]')
+        .addClass('active');
+    },
+    
+    _onMenuItemClick: function(event)  {
+      const clickedIndex = $(event.target).attr('data-slide-index');
+      this.horizontalSwiper.slideTo(clickedIndex);
+    }
+    
+  });
+  
+  $(document).on("deviceready", () => {
+    $(document.body).pakkasmarjaBerries();      
+  });
+
+})();
