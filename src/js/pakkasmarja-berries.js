0e31f7c948f920e615778e499679f041357fef23
Antti Leppä <antti.leppa@metatavu.fi>
2017-06-16 11:30:09 +0300

    Bootstrapping authentication system

--- src/js/pakkasmarja-berries.js
+++ src/js/pakkasmarja-berries.js
@@ -1,8 +1,32 @@
+/* global getConfig */
+
 (function(){
   'use strict';
   
   $.widget("custom.pakkasmarjaBerries", { 
     _create : function() {
+      const serverConfig = getConfig().server;
+      const secure = serverConfig.secure;
+      const host = serverConfig.host;
+      const port = serverConfig.port;
+      const httpProtocol = secure ? 'https' : 'http';
+      const wsProtocol = secure ? 'wss' : 'ws';
+      const serverUrl = httpProtocol + '://' + host + ':' + port;
+      const wsUrl = wsProtocol + '://' + host + ':' + port;
+      
+      this.element.on('authenticated', $.proxy(this._onAuthenticated, this));
+      this.element.on('joined', $.proxy(this._onJoined, this));
+      this.element.on('connect', $.proxy(this._onConnect, this));
+      
+      this.element.pakkasmarjaBerriesClient({
+        wsUrl: wsUrl,
+        serverUrl: serverUrl
+      });
+      
+      this.element.pakkasmarjaBerriesAuth({
+        serverUrl: serverUrl
+      });
+      
       this.element.pakkasmarjaBerriesChat();
       this.horizontalSwiper = new Swiper('.swiper-horizontal', {});
       this.verticalSwiper = new Swiper('.swiper-vertical', {
@@ -14,6 +38,13 @@
       });
       this.horizontalSwiper.on('onSlideChangeEnd', (swiper) => {this._onSlideChangeEnd(swiper); });
       $(this.element).find('.secondary-menu .menu-item').on('click', (event) => { this._onMenuItemClick(event); });
+      
+      
+      this.element.pakkasmarjaBerriesAuth('authenticate'); 
+    },
+    
+    sessionId: function () {
+      return this.element.pakkasmarjaBerriesAuth('sessionId');
     },
     
     _onSlideChangeEnd: function(swiper) {    
@@ -29,6 +60,19 @@
     _onMenuItemClick: function(event)  {
       const clickedIndex = $(event.target).attr('data-slide-index');
       this.horizontalSwiper.slideTo(clickedIndex);
+    },
+
+    _onAuthenticated: function () {
+      console.log("_onAuthenticated");
+      this.element.pakkasmarjaBerriesAuth('join');
+    },
+    
+    _onJoined: function () {
+      this.element.pakkasmarjaBerriesClient('connect', this.sessionId());
+    },
+    
+    _onConnect: function (event, data) {
+      
     }
     
   });
