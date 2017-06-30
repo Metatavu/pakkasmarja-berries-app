f4c7b9df931a0514ce0c22f880a06988e703963e
villekoivukangas <villekoivukangas@192.168.100.20>
2017-06-30 11:14:18 +0300

    Revert "Added hamburger menu closing function to back button"
    This reverts commit b08f9ab0158d7afbfef0ab55c56cd49e48de4cd4.

--- src/js/pakkasmarja-berries-device-controls.js
+++ src/js/pakkasmarja-berries-device-controls.js
@@ -23,23 +23,18 @@
     },
     
     _onBackButtonClick: function (event) {
-      if ($(".hamburger-menu").hasClass('menu-open')) {
-        $(".hamburger-menu").removeClass('menu-open');
-        $(".hamburger-menu").hide("slide", { direction: "right" }, 200);
+      if ($(".chat-container").hasClass("chat-conversation-open")) {
+        $(".chat-container").pakkasmarjaBerriesChatThread('leaveThread');
+        $('.questions-view').removeClass("question-group-threads");
+      } else if ($(".news-wrapper").hasClass("news-article-open")) {
+        $(".news-wrapper").removeClass("news-article-open");
+        $(document.body).pakkasmarjaBerriesNews("closeNews");
+      } else if ($('.questions-view').hasClass('question-group-threads')) {
+        $('.questions-view').removeClass("question-group-threads");
+        $(document.body).pakkasmarjaBerries("updateSwiper", 2);
+        $(document.body).pakkasmarjaBerries('restoreMainView');
       } else {
-        if ($(".chat-container").hasClass("chat-conversation-open")) {
-          $(".chat-container").pakkasmarjaBerriesChatThread('leaveThread');
-          $('.questions-view').removeClass("question-group-threads");
-        } else if ($(".news-wrapper").hasClass("news-article-open")) {
-          $(".news-wrapper").removeClass("news-article-open");
-          $(document.body).pakkasmarjaBerriesNews("closeNews");
-        } else if ($('.questions-view').hasClass('question-group-threads')) {
-          $('.questions-view').removeClass("question-group-threads");
-          $(document.body).pakkasmarjaBerries("updateSwiper", 2);
-          $(document.body).pakkasmarjaBerries('restoreMainView');
-        } else {
-          $(document.body).pakkasmarjaBerries("updateSwiper", 0); 
-        } 
+        $(document.body).pakkasmarjaBerries("updateSwiper", 0); 
       }
     }
     
