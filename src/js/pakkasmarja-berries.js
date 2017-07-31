16431cf430308c2b7205a6cb5831d97b44106697
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-07-31 15:09:11 +0300

    Bumped version

--- src/js/pakkasmarja-berries.js
+++ src/js/pakkasmarja-berries.js
@@ -115,7 +115,7 @@
         url: `${this._serverUrl}/version`,
         dataType: 'text',
         success: (version) => {
-          if (version != '1.2.3') {
+          if (version != '1.2.5') {
             navigator.notification.alert('Uusi versio sovelluksesta saatavilla.', () => {}, 'Uusi versio', 'OK');
           }
         }
